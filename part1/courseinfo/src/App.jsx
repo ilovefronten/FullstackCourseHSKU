a9760fcf50d9bd4cdd97aee7f24e130765bb3bcb
0xtopus <gsy@gsy123mailbox.com>
2024-11-10 12:48:11 +0800

    Finished Exercises 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -17,8 +17,8 @@ const Content = ({ parts, ...other }) => {
 }
 
 const Footer = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum+part.exercises, 0);
-  
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+
   return (
     <>
       <p>Number of exercises {total}</p>
@@ -28,29 +28,31 @@ const Footer = ({ parts }) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-  return (
+  return(
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
       
-      <Footer parts={parts} />
-    </div>
+      <Footer parts={course.parts} />
+    </div >
   )
 }
 
